368bdfe01c10171ea24c9a8cc40a8b391142316d
admirf <admir.ferhatovic@olx.ba>
2025-06-05 14:56:16 +0200

    make test use its own db

--- tests/Feature/ShippingTest.php
+++ tests/Feature/ShippingTest.php
@@ -2,11 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\Shipment;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ShippingTest extends TestCase
 {
+    use InteractsWithDatabase, RefreshDatabase, InteractsWithContainer;
+
     private const TRACKING_CODE = '1234567890';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Shipment::query()->create([
+            'tracking_code' => self::TRACKING_CODE,
+            'expected_delivery_at' => now()->addWeek()
+        ]);
+    }
 
     public function testShippingCsvShow(): void
     {
