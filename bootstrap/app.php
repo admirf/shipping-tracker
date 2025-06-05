7109bc0a47cb441c0d4a597639654e577cdf450c
admirf <admir.ferhatovic@olx.ba>
2025-06-05 13:10:22 +0200

    handle exceptions and define render for our shipment not found exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,12 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Shipping\Exceptions\ShipmentNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,5 +26,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+
+            return $request->expectsJson();
+        });
+
+        $exceptions->render(function (ShipmentNotFoundException $e, Request $request) {
+            abort(404, $e->getMessage());
+        });
     })->create();
