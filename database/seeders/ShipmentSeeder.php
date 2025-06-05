5745f259ffe9f2547ba816d49581d34e801786d4
admirf <admir.ferhatovic@olx.ba>
2025-06-05 12:44:54 +0200

    Add database seeder for dummy data and install command

---
+++ database/seeders/ShipmentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Shipment;
+use Illuminate\Database\Seeder;
+
+class ShipmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // add some dummy data
+        Shipment::query()->create([
+            'tracking_code' => '1234567890',
+            'expected_delivery_at' => now()->addWeek(),
+        ]);
+
+        Shipment::query()->create([
+            'tracking_code' => '000000000',
+            'expected_delivery_at' => now()->addWeek(),
+        ]);
+    }
+}
